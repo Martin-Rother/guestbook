4be2c4872c1128f868b91764876725b786470f12
Martin Rother <martin.rother@binaris-informatik.de>
2022-06-17 14:04:18 +0200

    add max size to entry

--- backend/database/migrations/2022_06_15_020835_create_posts_table.php
+++ backend/database/migrations/2022_06_15_020835_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('entry');
+            $table->string('entry', 4000);
             $table->timestamps();
         });
     }
